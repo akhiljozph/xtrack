7d61fb440b44870eb899b391e6a2eb1841fd8164
akhil.jozph <ajpulluvelil@gmail.com>
2024-10-01 10:58:06 +0530

    EX-6 - Added custom response for default route.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import Koa from "koa";
 import 'dotenv/config';
+import Koa from "koa";
 import mongoose from "mongoose";
 
 import logger from "./common/logger/logger";
 
-const app = new Koa();
 const PORT = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI || "";
 
+const app = new Koa();
+
 app.use(async (ctx) => {
-    ctx.body = "Hello World";
+    ctx.body = `
+        <section style='height:calc(100vh - 16px);display:flex;align-items:center;justify-content:center;'>
+            <label style='font-family:congenial;font-size:64px;font-weight:700;'>Welcome to Ex Tracker</label>
+        </section>
+    `;
 });
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(DB_URI);
+        await mongoose.connect(DB_URI);
         logger.info(`Successfully Connected to MongoDB`);
     } catch (error) {
         logger.error(error);
