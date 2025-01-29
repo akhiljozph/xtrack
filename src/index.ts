9e55dc7df62e6e2c05d8f1916b7b4a1072d1530b
akhil.jozph <ajpulluvelil@gmail.com>
2025-01-29 12:16:13 +0530

    EX-12 - PostgreSQL connection query added.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import koa from "koa";
 import parser from "koa-bodyparser";
 import cors from "@koa/cors";
+import { Client } from "pg";
 
 const app = new koa();
 
@@ -8,12 +9,24 @@ app.use(cors());
 app.use(parser());
 
 const PORT = process.env.PORT || 3000;
-const DB_URI = process.env.DB_URI || "";
+
+const client = new Client({
+    user: 'idgital',
+    host: 'localhost',
+    database: 'local_db_new',
+    password: 'Test1234',
+    port: 5432,
+});
 
 app.use(async ctx => {
-    ctx.body = "Hellow World";
+    ctx.body = "Hello World";
 });
 
-app.listen(PORT, () => {
-    console.log(`Ex-tracker is running on port ${PORT}`);
-});
+client.connect()
+    .then(() => {
+        console.log('Hurray, Connected to Database.');
+        app.listen(PORT, () => {
+            console.log(`Ex-tracker is running on port ${PORT}`);
+        });
+    })
+    .catch(err => console.error('Oops! Database connection error!', err.stack));
