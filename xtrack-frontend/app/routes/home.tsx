70788df5d0486aa14182c5ec64aa529842a8ac79
akhiljozph <ajpulluvelil@gmail.com>
2026-01-11 21:41:22 +0530

    XTRC-1 - Initialize frontend project with React.js, React Router, TypeScript and Tailwind CSS.

--- xtrack-frontend/app/routes/home.tsx
+++ xtrack-frontend/app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "XTRACK" },
+    { name: "description", content: "Smart expense tracker by AK with ❤️!" },
   ];
 }
 
