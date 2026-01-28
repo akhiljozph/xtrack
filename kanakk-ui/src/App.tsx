2343e539b6203a9c2f01501c28a701a5de139911
akhiljozph <ajpulluvelil@gmail.com>
2026-01-28 23:19:20 +0530

    KANK-4 - Implementation of sign-in component.

--- kanakk-ui/src/App.tsx
+++ kanakk-ui/src/App.tsx
@@ -1,31 +1,12 @@
 import './App.css'
-import Button from './components/ui/button/Button'
-import DropDown from './components/ui/drop-down/DropDown'
-import Input from './components/ui/input/Input'
-import MultiSelect from './components/ui/multi-select/MultiSelect'
+
+import SignIn from './pages/sign-in/SignIn'
 
 function App() {
 
-  const onClick = () => {
-    console.log('Click happened!')
-  }
-
-  const dropDownOptions = [
-    { name: "one", value: "one" },
-    { name: "two", value: "two" },
-  ]
-
-  const handleChange = (values: string) => {
-    console.log('Event', values);
-  }
-
   return (
     <div className='kanakk-lander'>
-      {/* Kanakk | Kanakkupusthakam
-      <Button onClick={(() => { onClick() })}>Sample</Button>
-      <DropDown options={dropDownOptions} />
-      <MultiSelect options={dropDownOptions} /> */}
-      <Input label='first name' type='text' placeholder='John' onChange={handleChange} />
+      <SignIn />
     </div>
   )
 }
--- kanakk-ui/src/pages/sign-in/SignIn.tsx
+++ kanakk-ui/src/pages/sign-in/SignIn.tsx
@@ -1,7 +1,18 @@
+import Input from "../../components/ui/input/Input";
+
 const SignIn = () => {
+
+    const handleChange = (value: string) => {
+        console.log(value)
+    }
+
     return (
-        <>
-        </>
+        <section>
+            <form>
+                <Input type="text" label="Username" placeholder="johndoe" onChange={handleChange} />
+                <Input type="text" label="Password" placeholder="johndoe" onChange={handleChange} />
+            </form>
+        </section>
     )
 }
 export default SignIn;
